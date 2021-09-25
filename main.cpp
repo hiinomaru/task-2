eb02afaa3a98243f6b1d9861509e356731f4c366
Starikov Igor <86251288+hiinomaru@users.noreply.github.com>
2021-09-25 15:55:30 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ struct Node{
     long ISBN;
     string full_name;
     string adress;
+    bool isOpen;
+    bool isDel;
 };
 
 class ListGenerator{
@@ -24,11 +26,11 @@ private:
                                "Washington", "Ninth", "Lake", "Hill"};
 public:
     void Gen(int size){
-        srand(1234);
+        srand(1233);
         ofstream out;
         out.open("/Users/igorstarikov/CLionProjects/СиАОД №2 вар. 1/cmake-build-debug/in.txt");
         for(int i = 0; i < size; i++){
-            out << to_string(random() % 90000 + 10000) << "@" << Name[rand() % 20] + " " +
+            out << to_string(rand() % 90000 + 10000) << "@" << Name[rand() % 20] + " " +
             SurName[rand() % 20] + "@" + Street[rand() % 20] + " Str. " + to_string(rand() % 200 + 1) + "\n";
         }
     }
@@ -46,9 +48,15 @@ public:
         size = S;
         table.clear();
         table.resize(size);
+        for(int i = 0; i < size; i++){
+            Node* N = new Node{0, "", "", true, false};
+            table[i] = N;
+        }
         for(int i = 0; i < temp.size(); i++){ // insert Nodes from old table to resized
-            if (temp[i] != nullptr)
+            if (!temp[i]->isOpen){
                 this->push(temp[i]);
+                amount++;
+            }
         }
         temp.clear();
     }
@@ -56,12 +64,18 @@ public:
     HashTable(int s){
         size = s;
         table.resize(s);
+        for(int i = 0; i< size; i++)
+        {
+            Node* N = new Node{0, "", "", true, false};
+            table[i] = N;
+        }
     }
 
     void push(Node* N){ // insert Node in the table
         int hash = this->hash(*N);
+        cout << hash;
         int first_hash = hash - 1; // we'll need it to check if there's a loop
-        while((table[hash] != nullptr) && (hash != first_hash))
+        while((!table[hash]->isOpen) && (first_hash != hash))
             hash = (hash + 1) % size;
         if(hash == first_hash) // if ended because of a loop
             std::cout << "PUSH ERROR, table need to be resized" << hash << "\n"; // needed resize
@@ -72,16 +86,8 @@ public:
     }
 
     void pop(int ISBN) { // takes Node, delete Node from the table
-        int hash = this->hash(ISBN);
-        int first_hash = hash - 1; // we'll need it to check if there's a loop
-        if (table[hash] != nullptr) {
-            while ((table[hash]->ISBN != ISBN) && (hash != first_hash))
-                hash = (hash + 1) % size;
-            this->table.erase(table.begin() + hash); // delete Node from the table
-            amount--;
-        }
-        else
-            cout << "POP ERROR, there's no node with ISBN = " << hash << "\n"; // needed resize
+        Node* N1 = new Node{0, "", "", true, true};
+        this->table[this->find(ISBN)] = N1;
     }
 
     const int hash(Node N){ // takes Node, returns hash number
@@ -98,23 +104,25 @@ public:
 
     void print(){ // console output
         for(int i = 0; i < size; i++) {
-            if (table[i] != nullptr)
+            if (!table[i]->isOpen)
                 std::cout << i + 1 << "     " << table[i]->ISBN << "     "
                 << table[i]->full_name << "     " << table[i]->adress << "\n";
         }
     }
 
-    Node* find(long ISBN){ // takes ISBN, returns corresponding Node
+    long find(long ISBN) { // takes ISBN, returns corresponding Node
         //auto t1 = std::chrono::high_resolution_clock::now(); // start time
         int hash = this->hash(ISBN);
         int first_hash = hash - 1; // we'll need it to check if there's a loop
-        while((table[hash]->ISBN != ISBN) && (hash != first_hash))
+        while ((table[hash]->ISBN != ISBN) && (hash != first_hash) &&
+               (!table[hash]->isDel) && (!table[hash]->isOpen)) {
             hash = (hash + 1) % size;
+        }
         //auto t2 = std::chrono::high_resolution_clock::now(); // end time
         //auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>( t2 - t1 ).count();
         // total time of the search execution, ns
         //std::cout << "time: " << duration << "\n"; console output time of the search execution
-        return this->table[hash];
+        return hash;
     }
 
     ~HashTable(){
@@ -150,7 +158,7 @@ public:
             string full_name = str.substr(str.find("@") + 1, str.rfind("@") - str.find("@") - 1); //parse str to get full_name
             string adress = str.substr(str.rfind("@") + 1); //parse str to get adress
             //std::cout << ISBN << "  " << full_name << "  " << adress <<"\n"; // you can print every parse result
-            Node* N = new Node{ISBN, full_name, adress}; //create new node
+            Node* N = new Node{ISBN, full_name, adress, false, false}; //create new node
             this->push(N); //push node
         }
         in.close(); //close filestream
@@ -180,30 +188,30 @@ int main() { // just remove /* ... */
     /*long ISBN = 12345;
     cout << "ISBN = " << ISBN << "\n" << "hash = " << Table->hash(ISBN);*/
     //2 read from file and push in the table
-    ListGenerator G0;
-    G0.Gen(3000);
+    /*ListGenerator G0;
+    G0.Gen(300);
     cout << Table->Info();
     Table->fill(0);
     Table->print();
-    cout << Table->Info();
+    cout << Table->Info();*/
     //3 pop from table and file
-    /*ListGenerator G1;
+    ListGenerator G1;
     G1.Gen(10);
     Table->fill(0);
     cout << "before\n";
     Table->print();
     cout << Table->Info();
-    Table->pop(96649);
+    Table->pop(18101);
     cout << "after\n";
     Table->print();
-    cout << Table->Info();*/
+    cout << Table->Info();
     //4 find using hashtable
     /*ListGenerator G2;
     G2.Gen(10);
     Table->fill(0);
     Table->print();
     cout << Table->Info();
-    Node* N = Table->find(96649);
+    Node* N = Table->find(18101);
     cout << "Found a node: " << N->ISBN << "  " << N->full_name << "  " << N->adress;*/
     //5 do rehash
     /*ListGenerator G3;
